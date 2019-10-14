46381b2231f16a0674a40efbd3396024f2fa930d
Siddhartha Basu <biosidd@gmail.com>
2019-10-14 16:51:28 -0500

    feat: add  getValidatorProto func to copy validation proto file from github repo

--- git.go
+++ git.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 
+	"github.com/google/go-github/v28/github"
+
+	"github.com/urfave/cli"
 	git "gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing"
 )
@@ -24,4 +29,37 @@ func cloneGitRepo(repo, branch string, isTag bool) (string, error) {
 	}
 	_, err = git.PlainClone(dir, false, cloneOpt)
 	return dir, err
+}
+
+func getValidatorProto(c *cli.Context) (string, error) {
+	dir, err := ioutil.TempDir(os.TempDir(), "validator")
+	if err != nil {
+		return "", fmt.Errorf("error in creating temp dir %s", err)
+	}
+	out := filepath.Join(dir, c.String("validator-proto-local-path"))
+	err = os.MkdirAll(out, 0774)
+	if err != nil {
+		return "", fmt.Errorf("error in creating validator-proto-path %s %s", out, err)
+	}
+	client := github.NewClient(nil)
+	ghf, _, _, err := client.Repositories.GetContents(
+		context.Background(),
+		c.String("validator-repo-owner"),
+		c.String("validator-repo-name"),
+		c.String("validator-proto-path"),
+		&github.RepositoryContentGetOptions{
+			Ref: c.String("validator-repo-version"),
+		})
+	if err != nil {
+		return "", fmt.Errorf("error in downloading file from given path %s", err)
+	}
+	ct, err := ghf.GetContent()
+	if err != nil {
+		return "", fmt.Errorf("error in getting content from file in repo %s", err)
+	}
+	out = filepath.Join(out, c.String("validator-proto-path"))
+	if err := ioutil.WriteFile(out, []byte(ct), 0644); err != nil {
+		return "", fmt.Errorf("error in writing content to file %s %s", out, err)
+	}
+	return dir, nil
 }
